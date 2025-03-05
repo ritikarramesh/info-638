d67427b0dafd80cb9c1ba3c8d0d4577903a6fba4
ritikarramesh <rramesh@pratt.edu>
2025-03-05 18:56:00 -0500

    duplicated file from a2 hw

---
+++ info-638-a2 copy/bookedin.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const indexRouter = require('./routes/index');
+const authorsRouter = require('./routes/authors');
+const booksRouter = require('./routes/books');
+const genresRouter = require('./routes/genres'); //assignment 2 addition
+
+const bodyParser = require('body-parser')
+
+var handlebars = require('express-handlebars').create({
+  helpers: {
+    eq: (v1, v2) => v1 == v2,
+    ne: (v1, v2) => v1 != v2,
+    lt: (v1, v2) => v1 < v2,
+    gt: (v1, v2) => v1 > v2,
+    lte: (v1, v2) => v1 <= v2,
+    gte: (v1, v2) => v1 >= v2,
+    and() {
+        return Array.prototype.every.call(arguments, Boolean);
+    },
+    or() {
+        return Array.prototype.slice.call(arguments, 0, -1).some(Boolean);
+    },
+    someId: (arr, id) => arr && arr.some(obj => obj.id == id),
+    in: (arr, obj) => arr && arr.some(val => val == obj),
+    dateStr: (v) => v && v.toLocaleDateString("en-US")
+  }
+});
+
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use('/', indexRouter);
+app.use('/authors', authorsRouter);
+app.use('/books', booksRouter);
+app.use('/genres', genresRouter); //assignment 2 addition
+
+app.use('/', function(req, res, next) {
+  res.send("<h1>Hello BookedIn</h1>");
+});
+
+app.use((req, res) => {
+    res.type('text/plain')
+    res.status(404)
+    res.send('<h1>404 - Not Found</h1>' )
+  })
+
+app.use((err, req, res, next) => {
+    console.error(err.message)
+    res.type('text/plain')
+    res.status(500)
+    res.send('500 - Server Error')
+})
+
+app.listen(port, () => console.log( //week 5 class note: app.listen basically says keep the code running -- 'we started a web server'
+  `Express started on http://localhost:${port}; ` +
+  `press Ctrl-C to terminate.`))
