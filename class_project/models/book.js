777369b9babcc5ddc38de94188356b2b85b30f4b
ritikarramesh <rramesh@pratt.edu>
2025-04-01 19:43:22 -0400

    cleaned up code

--- class_project/models/book.js
+++ class_project/models/book.js
@@ -1,9 +1,9 @@
 const books = [ //i assigned authorId and genreId so that no data was duplicated
-  {title: "Verity", publishingYear: 2022, authorIds: ["0"], genreId: [0]}, //for example: authorIds: ["0"] is index 0 in authors array (Colleen Hoover) and genreId [0] is index 0 in genres array (Psychological Thriller) (and so on for the rest) --
-  {title: "The Fault in Our Stars", publishingYear: 2012, authorIds: ["1"], genreId: [1]}, 
-  {title: "Twilight", publishingYear: 2005, authorIds: ["2"], genreId: [2]},
-  {title: "Dune", publishingYear: 1965, authorIds: ["3"], genreId: [3]},
-  {title: "Harry Potter and the Philosopher's Stone", publishingYear: 1997, authorIds: ["4"], genreId: [2]},
+  {title: "Verity", publishingYear: 2022, authorIds: ["0"], genreId: 0}, //for example: authorIds: ["0"] is index 0 in authors array (Colleen Hoover) and genreId [0] is index 0 in genres array (Psychological Thriller) (and so on for the rest) --
+  {title: "The Fault in Our Stars", publishingYear: 2012, authorIds: ["1"], genreId: 1}, 
+  {title: "Twilight", publishingYear: 2005, authorIds: ["2"], genreId: 2},
+  {title: "Dune", publishingYear: 1965, authorIds: ["3"], genreId: 3},
+  {title: "Harry Potter and the Philosopher's Stone", publishingYear: 1997, authorIds: ["4"], genreId: 2},
 ]
 
 exports.all = books
